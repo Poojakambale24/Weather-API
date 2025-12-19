47cabee96da4708404b4ed2b75c08b776b4244aa
Poojakambale24 <poojakambale234@gmail.com>
2025-12-19 20:59:33 +0530

    Configure HttpClient dependency injection for weather service

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 ﻿
 using CLOOPS.microservices;
+using Microsoft.Extensions.DependencyInjection;
 
 var app = new App();
+
+// Register HttpClient factory for external API calls
+app.builder.Services.AddHttpClient();
+
 await app.RunAsync();
